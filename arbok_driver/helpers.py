d91482380f68227a6f445d1de38650c14b7e9df2
RF2v meas pc <siqd@outlook.com>
2023-09-28 16:47:26 +1000

    Overhaul of measurement helper

--- arbok_driver/helpers.py
+++ arbok_driver/helpers.py
@@ -3,8 +3,6 @@ import copy
 import logging
 
 from qcodes.dataset import Measurement
-from qcodes.parameters import Parameter
-from qcodes.dataset.descriptions.detect_shapes import detect_shape_of_measurement
 
 from arbok_driver import Program
 
@@ -20,7 +18,7 @@ def register_opx_program_qc_params_on_measurement(
         program (Program): arbok_driver Program
         measurement (Measurement): QCoDeS measurement object
     Returns:
-
+        QCoDes measurement object: Measurement with registered parameters
     """
     if not hasattr(program, 'iteration'):
         program.add_parameter(
@@ -61,60 +59,10 @@ def run_qc_measurement_from_opx_program(
         dataset = datasaver.dataset
     return dataset
 
-def run_qdac_sweep_with_opx_program(
-    program: Program,
-    measurement: Measurement,
-    ext_settables: dict(),
-    shots: int
-    ):
-    """ 
-    Runs QCoDeS `measurement` based on specified settables and gettables on
-    the given opx `program` and and returns the resulting dataset with an
-    amount of `shots`
-    
-    Args:
-        program (Program): arbok_driver Program, parameterizing measurement
-        measurement (Measurement): qcodes measurement object
-        shots (int): amount of repetitions to be performed for averaging  
-    Returns:
-        dataset: QCoDeS dataset
-    """
-    if not hasattr(program, 'iteration'):
-        program.add_parameter(
-            'iteration', initial_value = 0, get_cmd = None, set_cmd = None)
-
-    measurement.register_parameter(program.iteration)
-    gettable_setpoints = [program.iteration]
-    for ext_settable, values in ext_settables.items():
-        logging.debug("Registering parameter: %s with value %s",
-                      ext_settable.name, values)
-        measurement.register_parameter(ext_settable)
-        gettable_setpoints.append(ext_settable)
-    for gettable in program.gettables:
-        logging.debug("Registering gettable: %s",
-                      gettable.name)
-        measurement.register_parameter(gettable, setpoints = gettable_setpoints)
-
-    with measurement.run() as datasaver:
-        for v1 in ext_settables.values()[0]:
-            ext_settables.items[1].set(v1)
-            for v2 in ext_settables.values()[1]:
-                ext_settables.items[1].set(v2)
-                for shot in range(shots):
-                    program.iteration.set(shot)
-                    result_args = [(program.iteration, program.iteration.get())]
-                    result_args.append(ext_settables.items[0], v1)
-                    result_args.append(ext_settables.items[1], v2)
-                    for gettable in program.gettables:
-                        result_args.append((gettable, gettable.get_raw(),))
-                    datasaver.add_result(*result_args)
-        dataset = datasaver.dataset
-    return dataset
-
 def run_qdac_measurement_from_opx_program(
     program: Program,
     measurement: Measurement,
-    ext_settables: list[dict],
+    sweep_list: list[dict],
     ):
     """ 
     Runs QCoDeS `measurement` based on specified settables and gettables on
@@ -124,48 +72,61 @@ def run_qdac_measurement_from_opx_program(
     Args:
         program (Program): arbok_driver Program, parameterizing measurement
         measurement (Measurement): qcodes measurement object
-        shots (int): amount of repetitions to be performed for averaging  
+        sweep_list (list[dict]): List of dictionairies with params as keys and
+            np.ndarrays as settables. Each list entry creates one sweep axis.
+            If you want to sweep params concurrently enter more entries into 
+            their sweep dict
     Returns:
         dataset: QCoDeS dataset
     """
-    def create_recursive_measurement_loop(ext_settables, result_args_temp):
+    def create_recursive_measurement_loop(sweeps_list_temp, result_args_temp):
         """
         Recursive generation of parameter sweeps for all axis given in dict
         ext_settables
         """
         result_args_temp = copy.copy(result_args_temp)
-        if not ext_settables:
-            # Innermost step: adding results of gettables and independent params
+        if not sweeps_list_temp:
             for gettable in program.gettables:
                 result_args_temp.append((gettable, gettable.get_raw(),))
+            result_args_temp += list(res_args_dict.values())
             datasaver.add_result(*result_args_temp)
             return
-        else:
-            settables = copy.copy(ext_settables)
-            sweep_data = (k := next(iter(settables)), settables.pop(k))
-            param, sweep_list = sweep_data
-            result_args_temp.append(None)
-            for value in sweep_list:
+        sweeps_list_temp = copy.copy(sweeps_list_temp)
+        sweep_dict = sweeps_list_temp.pop(0)
+        for idx in range(len(list(sweep_dict.values())[0])):
+            for param, values in sweep_dict.items():
+                value = values[idx]
                 logging.debug('Setting %s to %s', param.instrument.name, value)
                 param.set(value)
-                result_args_temp[-1] = (param, value)
-                create_recursive_measurement_loop(settables, result_args_temp)
+                try:
+                    _ = res_args_dict[f"{param.instrument}.{param.name}"]
+                    res_args_dict[f"{param.instrument}.{param.name}"] = (param, value)
+                except KeyError:
+                    logging.debug("Not registered")
+            create_recursive_measurement_loop(
+                    sweeps_list_temp, result_args_temp)
 
     gettable_setpoints = []
-    steps = []
-    for ext_settable, values in ext_settables.items():
-        print(ext_settable.name, values)
-        measurement.register_parameter(ext_settable)
-        print("register settable ", ext_settable)
-        gettable_setpoints.append(ext_settable)
-        steps.append(len(values))
-    for gettable in program.gettables:
-        print("SETPOINTS ", gettable_setpoints)
-        measurement.register_parameter(gettable, setpoints = gettable_setpoints)
-    print('STEPS! ', steps)
+    res_args_dict = {}
+    for i, sweep_dict in enumerate(sweep_list):
+        for i, param in enumerate(list(sweep_dict.keys())):
+            if i == 0:
+                logging.debug("First param of sweep ax: %s.%s",
+                              param.instrument, param.name)
+                res_args_dict[f"{param.instrument}.{param.name}"] = ()
+                measurement.register_parameter(param)
+                gettable_setpoints.append(param)
+            logging.debug("Register settable %s on axis %s", param.name, i)
+            
 
+    for gettable in program.gettables:
+        logging.debug("Register gettable %s", gettable_setpoints)
+        measurement.register_parameter(gettable, setpoints = gettable_setpoints)
+
+    # add_after_run (RESET QDAC VOLTS)
+    print(measurement.parameters)
     with measurement.run() as datasaver:
-        result_args = []#(program.iteration, program.iteration.get())]
-        create_recursive_measurement_loop(ext_settables, result_args)
+        result_args = []
+        create_recursive_measurement_loop(sweep_list, result_args)
         dataset = datasaver.dataset
     return dataset
