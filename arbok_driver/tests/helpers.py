54ab12ec9a2d1196ba2a3929ddab8ece518c3967
Andreas Nickl <59257257+andncl@users.noreply.github.com>
2023-10-09 18:03:51 +1100

    Changed measurement helper

--- arbok_driver/tests/helpers.py
+++ arbok_driver/tests/helpers.py
@@ -3,11 +3,11 @@ import numpy as np
 def set_sweeps_args(sequence) -> list:
     set_sweeps_args = [
         {
-            sequence.seq1.par1: np.zeros((5)),
-            sequence.seq2.par4: np.zeros((5)),
+            sequence.dummy_seq.sub_seq1.par1: np.zeros((5)),
+            sequence.dummy_seq.sub_seq2.par4: np.zeros((5)),
         },
         {
-            sequence.seq1.vHome_J1: np.zeros((7))
+            sequence.dummy_seq.sub_seq1.vHome_J1: np.zeros((7))
         },
     ]
     return set_sweeps_args
