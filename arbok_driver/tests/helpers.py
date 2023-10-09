de7b845b95b41895e9090ba0af7148570f38ac1c
andi <andreas@orangeqs.com>
2023-10-10 08:48:41 +1100

    Fixed test

--- arbok_driver/tests/helpers.py
+++ arbok_driver/tests/helpers.py
@@ -4,7 +4,7 @@ def set_sweeps_args(sequence) -> list:
     set_sweeps_args = [
         {
             sequence.dummy_seq.sub_seq1.par1: np.zeros((5)),
-            sequence.dummy_seq.sub_seq2.par4: np.zeros((5)),
+            sequence.dummy_seq.sub_seq2.par4: np.ones((5)),
         },
         {
             sequence.dummy_seq.sub_seq1.vHome_J1: np.zeros((7))
