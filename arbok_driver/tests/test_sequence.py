a086c5e8265768f9eab97ecd9e8dafa37152275d
Andreas Nickl <59257257+andncl@users.noreply.github.com>
2023-10-09 18:04:19 +1100

    Repurposed test_program -> sequence

--- arbok_driver/tests/test_sequence.py
+++ arbok_driver/tests/test_sequence.py
@@ -5,17 +5,20 @@ from qm import generate_qua_script
 from arbok_driver import Program
 from arbok_driver.tests.helpers import set_sweeps_args
 
-def test_set_sweeps(program_sequence) -> None:
-    program_sequence.set_sweeps(*set_sweeps_args(program_sequence))
-    assert len(program_sequence.sweeps) == 2
-    assert program_sequence.sweeps[0].length == 5
-    assert len(program_sequence.sweeps[0].parameters) == 2
-    assert program_sequence.sweep_size == 35
+def test_set_sweeps(dummy_sequence) -> None:
+    """"""
+    dummy_sequence.set_sweeps(*set_sweeps_args(dummy_sequence))
+    assert len(dummy_sequence.sweeps) == 2
+    assert dummy_sequence.sweeps[0].length == 5
+    assert len(dummy_sequence.sweeps[0].parameters) == 2
+    assert dummy_sequence.sweep_size == 35
 
 def test_qua_program_compilation_w_sweeps(
-        program_sequence) -> None:
-    program_sequence.set_sweeps(*set_sweeps_args(program_sequence))
-    qua_prog_str = program_sequence.qua_program_as_str
+        dummy_sequence) -> None:
+    """Tests whether the qua code is compiled correctly"""
+    print(dummy_sequence.submodules)
+    dummy_sequence.set_sweeps(*set_sweeps_args(dummy_sequence))
+    qua_prog_str = dummy_sequence.qua_program_as_str
     # we expect 8 declares: 2x3(2 per parameter -> sweep_arr + qua_var) 
     # + 2 as iterators for for loops (per sweep axis)
     assert len([m.start() for m in re.finditer('declare', qua_prog_str)]) == 8
