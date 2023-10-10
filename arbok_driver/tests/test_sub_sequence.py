72d08010a5d883639a0f7cd31f9d6a7bd83d2aaf
Andreas Nickl <59257257+andncl@users.noreply.github.com>
2023-10-10 13:50:17 +1100

    Overworked test_sub_sequence

--- arbok_driver/tests/test_sub_sequence.py
+++ arbok_driver/tests/test_sub_sequence.py
@@ -7,25 +7,30 @@ from qm import generate_qua_script
 from arbok_driver import SubSequence, Sample, SequenceParameter
 from arbok_driver.tests.helpers import set_sweeps_args
 
-def test_sequence_init(sub_sequence_1, sub_sequence_2) -> None:
+def test_sub_seq_param_init(sub_sequence_1, sub_sequence_2) -> None:
+    """Tests if parameters are correctly initialized from config"""
     assert sub_sequence_1.par1.get() == 1
     assert sub_sequence_2.par4.get() == 2
     assert len(sub_sequence_1.parameters) == 6
     assert len(sub_sequence_2.parameters) == 5
     assert isinstance(sub_sequence_1.par1.get(), int)
 
-def test_parent_child_sequence_behaviour(parent_sequence, sub_sequence_1) -> None:
-    assert parent_sequence.seq1.par2.get() == 10
-    assert parent_sequence.seq2.vHome_P1.get() == 0
+def test_parent_child_sequence_behaviour(
+        parent_sub_sequence, sub_sequence_1) -> None:
+    """Tests the behaviour of nested sub sequences"""
+    assert parent_sub_sequence.sub_seq1.par2.get() == 10
+    assert parent_sub_sequence.sub_seq2.vHome_P1.get() == 0
+    assert parent_sub_sequence.sub_seq1.parent_sequence == parent_sub_sequence
     with pytest.raises(KeyError):
-        parent_sequence.add_subsequence(sub_sequence_1)
-        print(parent_sequence.submodules)
+        parent_sub_sequence.add_subsequence(sub_sequence_1)
+        print(parent_sub_sequence.submodules)
 
 def test_qua_program_compilation_wo_sweeps(
-        program_sequence) -> None:
-    program_sequence.set_sweeps(*set_sweeps_args(program_sequence))
-    qua_prog_str = program_sequence.qua_program_as_str
-    print(qua_prog_str)
+        dummy_sequence) -> None:
+    """Tests if any sub sequence within a sequence that has sweeps compiles 
+    correctly to QUA code considering those sweeps """
+    dummy_sequence.set_sweeps(*set_sweeps_args(dummy_sequence))
+    qua_prog_str = dummy_sequence.dummy_parent_sub.get_qua_program_as_str()
     # we expect 8 declares: 2x3(2 per parameter -> sweep_arr + qua_var) 
     # + 2 as iterators for for loops (per sweep axis)
     assert len([m.start() for m in re.finditer('declare', qua_prog_str)]) == 8
@@ -33,8 +38,15 @@ def test_qua_program_compilation_wo_sweeps(
     assert len([m.start() for m in re.finditer('assign', qua_prog_str)]) == 3
     # we expect 2 one per sweep axis
     assert len([m.start() for m in re.finditer('for_', qua_prog_str)]) == 2
+    #breakpoint()
+    qua_prog_str = dummy_sequence.dummy_parent_sub.sub_seq1.get_qua_program_as_str()
+    assert len([m.start() for m in re.finditer('declare', qua_prog_str)]) == 8
+    assert len([m.start() for m in re.finditer('assign', qua_prog_str)]) == 3
+    assert len([m.start() for m in re.finditer('for_', qua_prog_str)]) == 2
 
-def test_add_qc_params_from_config(sub_sequence_1):
+
+def test_add_qc_params_from_config(sub_sequence_1) -> None:
+    """Test adding qc params by config"""
     sub_sequence_1.add_qc_params_from_config({
         'par8': {'unit': 'cycles', 'value': int(8)},
         'vRead': {'unit': 'V', 'elements': {'P1': 0.3, 'J1': 7, }},
@@ -46,3 +58,11 @@ def test_add_qc_params_from_config(sub_sequence_1):
         sub_sequence_1.add_qc_params_from_config({
             'par69': {'unit': 'cycles', 'toast': int(8)},
             })
+
+def test_find_parent_sequence(mock_program, dummy_sequence) -> None:
+    """Tests if parent sequence is found from sub sequences"""
+    mock_program.add_sequence(dummy_sequence)
+    parent = mock_program.dummy_sequence.parent_sequence
+    assert parent == mock_program.dummy_sequence
+    parent = mock_program.dummy_sequence.dummy_parent_sub.parent_sequence
+    assert parent == mock_program.dummy_sequence
