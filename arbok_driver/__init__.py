b19da6ca58635c3e178d5ce088be179d414a67f1
RF2v meas pc <siqd@outlook.com>
2023-10-25 13:39:01 +1100

    Added sequence to init

--- arbok_driver/__init__.py
+++ arbok_driver/__init__.py
@@ -3,6 +3,7 @@ from .program import Program
 from .read_sequence import ReadSequence
 from .readout_helper import Readout
 from .sample import Sample
+from .sequence import Sequence
 from .sequence_parameter import SequenceParameter
 from .sub_sequence import SubSequence
 from .sweep import Sweep
