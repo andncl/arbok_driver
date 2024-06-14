7c59c7d16e2601c162be45b71a193ff1589d5a3c
Matt Flax <flatmax@diraq.com>
2024-06-14 11:48:49 +1000

    update qua programs post fixes

--- docs/qua_programs/tut1_multi_param_sweeps.py
+++ docs/qua_programs/tut1_multi_param_sweeps.py
@@ -1,25 +1,37 @@
 
-# Single QUA script generated at 2024-04-12 13:24:54.416459
+# Single QUA script generated at 2024-06-14 11:36:05.560896
 # QUA library version: 1.1.7
 
 from qm.qua import *
 
 with program() as prog:
-    v1 = declare(int, )
+    v1 = declare(fixed, )
     v2 = declare(int, )
-    a1 = declare(int, value=[10, 20, 30, 40])
     v3 = declare(int, )
-    a2 = declare(int, value=[10, 20, 30, 40])
-    v4 = declare(int, )
+    v4 = declare(int, value=0)
     v5 = declare(int, )
+    v6 = declare(int, )
     with infinite_loop_():
         pause()
-        with for_each_((v2,v3),(a1,a2)):
-            with for_(v1,0,(v1<1.125),(v1+0.25)):
+        assign(v2, 10)
+        assign(v3, 10)
+        with for_(v5,0,(v5<4),(v5+1)):
+            assign(v2, (v2+10))
+            assign(v3, (v3+10))
+            align()
+            assign(v1, 0.1)
+            with for_(v6,0,(v6<5),(v6+1)):
+                assign(v1, (v1+0.225))
+                align()
                 align()
                 play("ramp"*amp(v1), "gate_1", duration=v3)
                 wait(v2, "gate_1")
                 play("ramp"*amp((0-v1)), "gate_1", duration=v3)
+                assign(v4, (v4+1))
+                r1 = declare_stream()
+                save(v4, r1)
+    with stream_processing():
+        r1.buffer(1).save("dummy_squence_shots")
 
 
 config = None
--- docs/qua_programs/tut1_square_pulses.py
+++ docs/qua_programs/tut1_square_pulses.py
@@ -1,16 +1,22 @@
 
-# Single QUA script generated at 2024-04-12 13:24:54.204208
+# Single QUA script generated at 2024-06-14 11:36:04.777579
 # QUA library version: 1.1.7
 
 from qm.qua import *
 
 with program() as prog:
+    v1 = declare(int, value=0)
     with infinite_loop_():
         pause()
         align()
         play("ramp"*amp(0.5), "gate_1", duration=20)
         wait(50, "gate_1")
         play("ramp"*amp(-0.5), "gate_1", duration=20)
+        assign(v1, (v1+1))
+        r1 = declare_stream()
+        save(v1, r1)
+    with stream_processing():
+        r1.buffer(1).save("dummy_squence_shots")
 
 
 config = None
