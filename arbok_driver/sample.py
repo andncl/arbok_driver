0f0f3e951fd13c57691784501fc84b8555afd6f8
Matt Flax <flatmax@flatmax.org>
2024-11-28 13:57:44 +1100

    Sample add a master_config_path setter and getter

--- arbok_driver/sample.py
+++ arbok_driver/sample.py
@@ -4,6 +4,9 @@ class Sample():
     """
     Class describing the used sample by its config and the used sequence. 
     """
+
+    _master_config_path: str = None
+
     def __init__(self, name: str, opx_config: dict, divider_config: dict):
         """
         Constructor class for 'Sample' class.
@@ -16,4 +19,25 @@ class Sample():
         self.name = name
         self.config = opx_config
         self.divider_config = divider_config
-        self.elements = list(self.config['elements'].keys())
+        # self.elements = list(self.config['elements'].keys())
+
+    @property
+    def master_config_path(self):
+        """
+        Getter for master_config.
+        Returns the current configuration dictionary.
+        """
+        return self._master_config_path
+
+    @master_config_path.setter
+    def master_config_path(self, config_path):
+        """
+        Setter for master_config.
+        Validates and sets the configuration path as a string.
+
+        :param config_path: A dictionary containing configuration data.
+        :raises ValueError: If the config is not a dictionary.
+        """
+        if not isinstance(config_path, str):
+            raise ValueError("master_config must be a str.")
+        self._master_config_path = config_path
