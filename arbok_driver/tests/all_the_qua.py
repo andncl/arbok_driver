399ad6cbe58b82f6e6da9ee997b96bf1142e7be8
Matt Flax <flatmax@flatmax.org>
2025-02-20 14:59:31 +1100

    AllTheQua added

--- arbok_driver/tests/all_the_qua.py
+++ arbok_driver/tests/all_the_qua.py
@@ -1,12 +1,12 @@
 from qm import qua
-from arbok_driver import SubSequence
+from arbok_driver import ReadSequence
 
-class AllTheQua(SubSequence):
+class AllTheQua(ReadSequence):
+    
     def qua_declare(self):
         print('qua_declare')
-        # comment
+        self.dummy_var.set(qua.declare(qua.fixed, 0.))
         self.qua_declare_atq = qua.declare(int)
-        self.qua_declare_atq_stream = qua.declare_stream()
 
     def qua_before_sweep(self):
         qua.assign(self.qua_declare_atq, 0)
@@ -17,11 +17,10 @@ class AllTheQua(SubSequence):
     def qua_sequence(self):
         qua.assign(self.qua_declare_atq, 2)
         qua.align(*self.elements)
+        for _ , readout in self.var_readouts.items():
+            readout.qua_measure_and_save()
 
     def qua_after_sequence(self):
         qua.assign(self.qua_declare_atq, 3)
+        qua.assign(self.dummy_var.get_raw(), self.dummy_var.get_raw()+0.0001)
         qua.align()
-        qua.save(self.qua_declare_atq, self.qua_declare_atq_stream)
-
-    def qua_stream(self):
-        self.qua_declare_atq_stream.save_all('atq_stream')
