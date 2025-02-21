a722121a5a3510e46cf76e4c79cddaf93abeb6c2
Matt Flax <flatmax@flatmax.org>
2025-02-21 15:52:28 +1100

    added the opx1000 config file

---
+++ arbok_driver/tests/qm_config/opx1000.py
@@ -0,0 +1,87 @@
+"""
+QUA-Config supporting OPX1000
+"""
+
+sampling_rate = int(1e9) # Hz
+T = 5e-3 # s
+
+config = {
+    "version": 1,
+    "controllers": {
+        'con1': {
+            "type": "opx1000",
+            "fems": {
+                1: {
+                    "type": "LF",
+                    "analog_outputs": {
+                        1: {
+                            "offset": 0.0,
+                            "output_mode": "direct",
+                            "sampling_rate": sampling_rate,
+                            "upsampling_mode": "mw",
+                        },
+                        2: {
+                            "offset": 0.0,
+                            "output_mode": "direct",
+                            "sampling_rate": sampling_rate,
+                            "upsampling_mode": "mw",
+                        },
+                    },
+                    "digital_outputs": {},
+                    "analog_inputs": {
+                        1: {"offset": 0.0, "gain_db": 0, "sampling_rate": sampling_rate},
+                        2: {"offset": 0.0, "gain_db": 0, "sampling_rate": sampling_rate},
+                    },
+                }
+            },
+        }
+    },
+    "elements": {
+        "output": {
+            "singleInput": {
+                "port": ("con1", 1, 1),
+            },
+            "operations": {
+                "cw": "const_pulse",
+            },
+        },
+        "loopback": {
+            "singleInput": {
+                "port": ("con1", 1),
+            },
+            "operations": {
+                "readout": "readout_pulse",
+            },
+            "outputs": {
+                "out1": ("con1", 1, 1),
+                "out2": ("con1", 1, 2),
+            },
+            "time_of_flight": 180,
+            "smearing": 0,
+        },
+    },
+    "pulses": {
+        "const_pulse": {
+            "operation": "control",
+            "length": 100,
+            "waveforms": {
+                "single": "const_wf",
+            },
+        },
+        "readout_pulse": {
+            "operation": "measurement",
+            "length": int(T*1e9), # ns
+            "waveforms": {
+                "single": "readout_wf",
+            },
+            "digital_marker": "ON",
+        },
+    },
+    "waveforms": {
+        "const_wf": {"type": "constant", "sample": 0.249},
+        "readout_wf": {"type": "constant", "sample": 0}
+    },
+    "digital_waveforms": {
+        "ON": {"samples": [(1, 0)]},
+    },
+}
