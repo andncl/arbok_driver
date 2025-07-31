373290455457aede69c41507400ec273ef029681
Andreas Nickl <59257257+andncl@users.noreply.github.com>
2025-07-31 11:18:12 +1000

    Updating some tests

--- arbok_driver/tests/test_arbok_driver.py
+++ arbok_driver/tests/test_arbok_driver.py
@@ -1,20 +1,20 @@
 """Module testing the Program class"""
 import pytest
-from arbok_driver import Sequence, ArbokDriver, Device
+from arbok_driver import Measurement, ArbokDriver, Device
 
 def test_arbok_driver_init(arbok_driver)-> None:
     """Tests if arbok_driver is correctly initialized"""
     assert arbok_driver.name == 'arbok_driver'
     assert arbok_driver.device.name == 'dummy_device'
-    assert arbok_driver.sequences == []
+    assert arbok_driver.measurements == []
 
-def test_arbok_driver_adding_sequence(arbok_driver, dummy_device)-> None:
-    """Tests if sequence is correctly added to arbok_driver"""
-    seq = Sequence(arbok_driver, 'dummy_sequence', dummy_device)
-    assert len(arbok_driver.sequences) == 1
-    assert arbok_driver.sequences[0] == seq
+def test_arbok_driver_adding_measurement(arbok_driver, dummy_device)-> None:
+    """Tests if measurement is correctly added to arbok_driver"""
+    meas = Measurement(arbok_driver, 'dummy_measurement', dummy_device)
+    assert len(arbok_driver.measurements) == 1
+    assert arbok_driver.measurements[0] == meas
     assert len(arbok_driver.submodules) == 1
 
-    arbok_driver.reset_sequences()
-    assert len(arbok_driver.sequences) == 0
+    arbok_driver.reset_measurements()
+    assert len(arbok_driver.measurements) == 0
     assert len(arbok_driver.submodules) == 0
--- arbok_driver/tests/test_sequence.py
+++ arbok_driver/tests/test_sequence.py
@@ -3,16 +3,16 @@ import numpy as np
 from qm import generate_qua_script
 from sklearn import dummy
 
-from arbok_driver import Sequence, SubSequence
+from arbok_driver import Measurement, SubSequence
 from arbok_driver.tests.helpers import set_sweeps_args
 
 def test_sequence_init(arbok_driver, dummy_device) -> None:
     """Tests if sequence is correctly initialized"""
-    seq = Sequence(arbok_driver, 'dummy_sequence', dummy_device)
-    assert seq.name == f'{arbok_driver.name}_dummy_sequence'
+    seq = Measurement(arbok_driver, 'dummy_measurement', dummy_device)
+    assert seq.name == f'{arbok_driver.name}_dummy_measurement'
 
 def test_sequence_sub_sequence_addition(
-        dummy_sequence, dummy_device) -> None:
+        dummy_measurement, dummy_device) -> None:
     """Tests if subsequence is correctly added to sequence"""
     config_1 = {
         'par1': {'unit': 'cycles', 'value': int(1)},
@@ -20,30 +20,30 @@ def test_sequence_sub_sequence_addition(
         'par3': {'unit': 'cycles', 'value': 1.1},
         'vHome': {'unit': 'V', 'elements': {'P1': 5, 'J1': 6, }},
     }
-    seq1 = SubSequence(dummy_sequence, 'seq1', dummy_device, config_1)
-    assert len(dummy_sequence.submodules) == 1
-    assert len(dummy_sequence.sub_sequences) == 1
-    assert dummy_sequence.sub_sequences[0] == seq1
-    assert hasattr(dummy_sequence, 'seq1')
-    assert dummy_sequence.seq1 == seq1
+    seq1 = SubSequence(dummy_measurement, 'seq1', dummy_device, config_1)
+    assert len(dummy_measurement.submodules) == 1
+    assert len(dummy_measurement.sub_sequences) == 1
+    assert dummy_measurement.sub_sequences[0] == seq1
+    assert hasattr(dummy_measurement, 'seq1')
+    assert dummy_measurement.seq1 == seq1
 
-def test_set_sweeps(sub_sequence_1, dummy_sequence) -> None:
+def test_set_sweeps(sub_sequence_1, dummy_measurement) -> None:
     """Tests if sweeps are correctly set"""
-    dummy_sequence.set_sweeps({sub_sequence_1.par1: np.arange(10)},
+    dummy_measurement.set_sweeps({sub_sequence_1.par1: np.arange(10)},
                               {sub_sequence_1.par2: np.arange(5)})
-    assert len(dummy_sequence.sweeps) == 2
-    assert dummy_sequence.sweeps[0].length == 10
-    assert dummy_sequence.sweeps[1].length == 5
-    assert len(dummy_sequence.sweeps[0].parameters) == 1
-    assert dummy_sequence.sweep_size == 50
+    assert len(dummy_measurement.sweeps) == 2
+    assert dummy_measurement.sweeps[0].length == 10
+    assert dummy_measurement.sweeps[1].length == 5
+    assert len(dummy_measurement.sweeps[0].parameters) == 1
+    assert dummy_measurement.sweep_size == 50
 
 # def test_qua_program_compilation_w_sweeps(
-#         dummy_sequence) -> None:
+#         dummy_measurement) -> None:
 #     """Tests whether the qua code is compiled correctly"""
-#     print(dummy_sequence.submodules)
-#     dummy_sequence.set_sweeps(*set_sweeps_args(dummy_sequence))
-#     qua_prog_str = dummy_sequence.get_qua_program_as_str()
-#     dummy_sequence.print_qua_program_to_file('test_output.txt')
+#     print(dummy_measurement.submodules)
+#     dummy_measurement.set_sweeps(*set_sweeps_args(dummy_measurement))
+#     qua_prog_str = dummy_measurement.get_qua_program_as_str()
+#     dummy_measurement.print_qua_program_to_file('test_output.txt')
 #     # we expect 8 declares: 2x3(2 per parameter -> sweep_arr + qua_var)
 #     # + 2 as iterators for for loops (per sweep axis)
 #     assert len([m.start() for m in re.finditer('declare', qua_prog_str)]) == 8
