b8bd5335e7044533ec498eb671b7a53cc0dc1131
andncl <andreaskochnickl@web.de>
2025-12-14 03:49:32 +0000

    Added notimplemented error for snake scan-reshaping

--- arbok_driver/gettable_parameter.py
+++ arbok_driver/gettable_parameter.py
@@ -160,7 +160,11 @@ class GettableParameter(ParameterWithSetpoints):
                 f"GettableParameter {self.name} has no data in the buffer. "
                 "Make sure the QUA program has run and the buffer is full."
             )
-        return data #= self._reshape_data(data, self.sweep_dims, self.snaked)
+        if any([x.snake_scan for x in self.measurement.sweeps]):
+            raise NotImplementedError(
+                "Snaked data reshaping is not yet implemented."
+            )
+        return data
 
     def _fetch_opx_buffer(self):
         """
