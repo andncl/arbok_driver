5b55bf2964899b1d877c66efd8f9e11246126619
andncl <andreaskochnickl@web.de>
2026-01-16 06:26:17 +0000

    Fixed sequence test

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -1,6 +1,11 @@
+import re
 import numpy as np
+from qm import generate_qua_script
 
 from arbok_driver import Measurement, SubSequence
+from arbok_driver.parameter_types import (
+    Amplitude, Time, Voltage
+)
 
 def test_sequence_init(arbok_driver, dummy_device) -> None:
     """Tests if sequence is correctly initialized"""
@@ -11,12 +16,20 @@ def test_sequence_sub_sequence_addition(
         dummy_measurement, dummy_device) -> None:
     """Tests if subsequence is correctly added to sequence"""
     config_1 = {
-        'par1': {'unit': 'cycles', 'value': int(1)},
-        'par2': {'unit': 'cycles', 'value': int(10)},
-        'par3': {'unit': 'cycles', 'value': 1.1},
-        'vHome': {'unit': 'V', 'elements': {'P1': 5, 'J1': 6, }},
+        'parameters':{
+            'par1': {'type': Time, 'value': int(100)},
+            'par2': {'type': Time, 'value': int(10)},
+            'par3': {'type': Voltage, 'value': 1.1},
+            'vHome': {'type': Amplitude, 'elements': {
+                'P1': 0.5, 'J1': 0.6, }},
+        }
     }
-    seq1 = SubSequence(dummy_measurement, 'seq1', dummy_device, config_1)
+    seq1 = SubSequence(
+        parent = dummy_measurement,
+        name = 'seq1',
+        device = dummy_device,
+        sequence_config = config_1
+        )
     assert len(dummy_measurement.submodules) == 1
     assert len(dummy_measurement.sub_sequences) == 1
     assert dummy_measurement.sub_sequences[0] == seq1
@@ -25,25 +38,57 @@ def test_sequence_sub_sequence_addition(
 
 def test_set_sweeps(sub_sequence_1, dummy_measurement) -> None:
     """Tests if sweeps are correctly set"""
-    dummy_measurement.set_sweeps({sub_sequence_1.par1: np.arange(10)},
-                              {sub_sequence_1.par2: np.arange(5)})
+    dummy_measurement.set_sweeps(
+        {sub_sequence_1.par1: np.arange(10)},
+        {sub_sequence_1.par2: np.arange(5)}
+        )
     assert len(dummy_measurement.sweeps) == 2
     assert dummy_measurement.sweeps[0].length == 10
     assert dummy_measurement.sweeps[1].length == 5
     assert len(dummy_measurement.sweeps[0].parameters) == 1
     assert dummy_measurement.sweep_size == 50
 
-# def test_qua_program_compilation_w_sweeps(
-#         dummy_measurement) -> None:
-#     """Tests whether the qua code is compiled correctly"""
-#     print(dummy_measurement.submodules)
-#     dummy_measurement.set_sweeps(*set_sweeps_args(dummy_measurement))
-#     qua_prog_str = dummy_measurement.get_qua_program_as_str()
-#     dummy_measurement.print_qua_program_to_file('test_output.txt')
-#     # we expect 8 declares: 2x3(2 per parameter -> sweep_arr + qua_var)
-#     # + 2 as iterators for for loops (per sweep axis)
-#     assert len([m.start() for m in re.finditer('declare', qua_prog_str)]) == 8
-#     # we expect 3, one per parameter in total
-#     assert len([m.start() for m in re.finditer('assign', qua_prog_str)]) == 3
-#     # we expect 2 one per sweep axis
-#     assert len([m.start() for m in re.finditer('for_', qua_prog_str)]) == 2
+def test_qua_program_compilation_w_linear_sweeps(
+        sub_sequence_1, dummy_measurement) -> None:
+    """Tests whether the qua code is compiled correctly"""
+    dummy_measurement.set_sweeps(
+        {sub_sequence_1.par1: np.arange(0, 10, 1)},
+        {sub_sequence_1.vHome_P1: np.arange(-0.1, 0.1, 0.1)}
+        )
+    qua_prog_str = dummy_measurement.get_qua_program_as_str()
+    qua_prog = dummy_measurement.get_qua_program()
+    qua_prog_str = generate_qua_script(qua_prog)
+    # we expect 5 declares: 2x2 (2 per parameter -> iterator + qua_var)
+    # + 1 to track the shots (always there!)
+    assert len([m.start() for m in re.finditer('declare\(', qua_prog_str)]) == 5
+    # we expect 8:
+    # 4 =2x2 (2 per parameter, initial val and increment)
+    # +2 for shot tracker (init and increment)
+    # +2 for each sweep axis
+    assert len([m.start() for m in re.finditer('assign', qua_prog_str)]) == 8
+    # we expect 2 one per sweep axis
+    assert len([m.start() for m in re.finditer('while_', qua_prog_str)]) == 2
+
+def test_qua_program_compilation_w_non_linear_sweeps(
+        sub_sequence_1, dummy_measurement) -> None:
+    """Tests whether the qua code is compiled correctly"""
+    dummy_measurement.set_sweeps(
+        {sub_sequence_1.par2: np.arange(0, 10, 1)},
+        {sub_sequence_1.vHome_P1: np.random.rand(8)}
+        )
+    qua_prog_str = dummy_measurement.get_qua_program_as_str()
+    qua_prog = dummy_measurement.get_qua_program()
+    qua_prog_str = generate_qua_script(qua_prog)
+    print(qua_prog_str)
+    # we expect 5 declares: 2x2 (2 per parameter -> iterator + qua_var)
+    # +1 extra for declaring the explicit array (of non-lin sweep)
+    # + 1 to track the shots (always there!)
+    assert len([m.start() for m in re.finditer('declare\(', qua_prog_str)]) == 6
+    # we expect 8:
+    # 4 =2x2 (2 per parameter, initial val and increment)
+    # +2 for shot tracker (init and increment)
+    # +2 for each sweep axis
+    assert len([m.start() for m in re.finditer('assign', qua_prog_str)]) == 8
+    # we expect 2 one per sweep axis
+    assert len([m.start() for m in re.finditer('while_', qua_prog_str)]) == 2
+
