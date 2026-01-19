08cafccd2096b38e620b77c32d4352e84038a936
andncl <andreaskochnickl@web.de>
2026-01-19 03:05:08 +0000

    Updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
 from dataclasses import dataclass
 import pytest 
 
-from arbok_driver import ArbokDriver, Device, SubSequence, Measurement, parameter_types
+from arbok_driver import (
+    ArbokDriver,
+    Device,
+    Measurement,
+    ParameterClass,
+    ReadSequence,
+    SubSequence,
+)
+from arbok_driver.parameter_types import (
+    Amplitude, Int, Voltage, ParameterMap
+)
 from .dummy_opx_config import dummy_qua_config
 
 opx_scale = 2
@@ -17,13 +27,13 @@ divider_config = {
     }
 }
 
-@dataclass
-class MeasurementParameterClass:
-    iteration: parameter_types.Int
 
-@dataclass
-class SubSequenceParameterClass:
-    par1: parameter_types.Amplitude
+@dataclass(frozen = True)
+class SubSequenceParameterClass(ParameterClass):
+    par1: Amplitude
+    par2: Amplitude
+    iteration: Int
+    v_home: ParameterMap[str, Voltage]
 
 @pytest.fixture
 def dummy_device():
@@ -44,9 +54,9 @@ def arbok_driver(dummy_device):
 @pytest.fixture
 def dummy_measurement(arbok_driver, dummy_device):
     """Returns dummy measurement instance"""
-    CustomMeasurement = Measurement
-    CustomMeasurement.PARAMETER_CLASS = MeasurementParameterClass
-    measurement = Measurement(arbok_driver, 'dummy_measurement', dummy_device)
+    measurement = Measurement(
+        arbok_driver, 'dummy_measurement', dummy_device,
+        sequence_config= {'iteration': {'type': Int, 'value': 9}})
     yield measurement
     arbok_driver.submodules.pop(measurement.full_name)
     arbok_driver.measurements.remove(measurement)
@@ -56,26 +66,41 @@ def dummy_measurement(arbok_driver, dummy_device):
 def sub_sequence_1(dummy_measurement, dummy_device):
     """Returns dummy subsequence with a few parameters"""
     config_1 = {
-        'par1': {'type': parameter_types.Int, 'value': int(1)},
-        'par2': {'type': parameter_types.Int, 'value': int(10)},
-        'par3': {'type': parameter_types.Voltage, 'value': 1.1},
-        'vHome': {'type': parameter_types.Voltage,
-        'elements': {'P1': 5, 'J1': 6, }},
+        'par1': {'type': Int, 'value': int(1)},
+        'par2': {'type': Int, 'value': int(10)},
+        'par3': {'type': Voltage, 'value': 1.1},
+        'v_home': {'type': Voltage, 'elements': {'P1': 5, 'J1': 6, }},
     }
     SubSequence.PARAMETER_CLASS = SubSequenceParameterClass
     seq1 = SubSequence(dummy_measurement, 'sub_seq1', dummy_device, config_1)
     yield seq1
-    #seq1.__del__()
 
 @pytest.fixture
 def sub_sequence_2(dummy_measurement, dummy_device):
     """Returns dummy subsequence with a few parameters"""
     config_2 = {
-        'par4': {'type': parameter_types.Int, 'value': int(2)},
-        'par5': {'type': parameter_types.Int, 'value': int(3)},
-        'vHome': {'type': parameter_types.Voltage, 'elements': {'P1': 0, 'J1': 7, }},
+        'par4': {'type': Int, 'value': int(2)},
+        'par5': {'type': Int, 'value': int(3)},
+        'v_home': {'type': Voltage, 'elements': {'P1': 0, 'J1': 7, }},
     }
     SubSequence.PARAMETER_CLASS = SubSequenceParameterClass
     seq2 = SubSequence(dummy_measurement, 'sub_seq2', dummy_device, config_2)
     yield seq2
     seq2.__del__()
+
+@pytest.fixture
+def read_sequence_no_readouts(dummy_measurement, dummy_device):
+    """Returns dummy subsequence with a few parameters"""
+    config_1 = {
+        'parameters': {
+            'par1': {'type': Int, 'value': int(1)},
+            'par2': {'type': Int, 'value': int(10)},
+            'par3': {'type': Voltage, 'value': 1.1},
+            'v_home': {'type': Voltage, 'elements': {'P1': 5, 'J1': 6, }},
+        },
+        'signals': [],
+        'readout_groups': {}
+    }
+    ReadSequence.PARAMETER_CLASS = SubSequenceParameterClass
+    seq1 = ReadSequence(dummy_measurement, 'read_seq', dummy_device, config_1)
+    yield seq1
