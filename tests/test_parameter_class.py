37b0e295da47c1fd232f5876953fa4a20e6c2f71
andncl <andreaskochnickl@web.de>
2026-01-19 03:42:42 +0000

    Adding test for parameter_class

---
+++ tests/test_parameter_class.py
@@ -0,0 +1,41 @@
+"""Testing that the a sub-sequences parameter class is added correctly"""
+import pytest
+from arbok_driver import SubSequence
+from arbok_driver.parameter_types import (
+    Int, Voltage, ParameterMap
+)
+from .conftest import UserSubSequence
+
+def test_missing_parameter_class() -> None:
+    with pytest.raises(TypeError):
+        class SubSequenceNoParameterClass(SubSequence):
+            pass
+
+def test_missing_parameter(dummy_measurement, dummy_device) -> None:
+    config_1 = {
+        'par2': {'type': Int, 'value': int(10)},
+        'par3': {'type': Voltage, 'value': 1.1},
+        'v_home': {'type': Voltage, 'elements': {'P1': 5, 'J1': 6, }},
+    }
+    with pytest.raises(TypeError):
+        _ = UserSubSequence(
+            dummy_measurement, 'sub_seq1', dummy_device, config_1)
+
+def test_parameter_mapping_on_sub_sequence(empty_sub_seq_1) -> None:
+    arbok_params = empty_sub_seq_1.arbok_params
+    print(arbok_params.__dict__)
+    assert len(arbok_params.__dict__) == 4
+    assert hasattr(arbok_params, 'par1')
+    assert isinstance(arbok_params.v_home, ParameterMap)
+    assert not hasattr(arbok_params, 'par3')
+    assert 'par3' in empty_sub_seq_1.parameters
+
+def test_parameter_mapping_on_read_sequence(read_sequence_no_readouts) -> None:
+    arbok_params = read_sequence_no_readouts.arbok_params
+    print(arbok_params.__dict__)
+    assert len(arbok_params.__dict__) == 4
+    assert hasattr(arbok_params, 'par1')
+    assert isinstance(arbok_params.v_home, ParameterMap)
+    assert arbok_params.v_home["P1"].get() == 5
+    assert not hasattr(arbok_params, 'par3')
+    assert 'par3' in read_sequence_no_readouts.parameters
