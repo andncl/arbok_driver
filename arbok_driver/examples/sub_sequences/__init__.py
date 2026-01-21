0aadbde6fabb682ed984c7c2b298cf06271d1008
andncl <andreaskochnickl@web.de>
2026-01-21 03:12:00 +0000

    Addedd square_pulse_legacy subsequence

--- arbok_driver/examples/sub_sequences/__init__.py
+++ arbok_driver/examples/sub_sequences/__init__.py
@@ -2,3 +2,4 @@ from .coulomb_peaks import CoulombPeaks
 from .stability_map import StabilityMap
 from .square_pulse import SquarePulse
 from .square_pulse_scalable import SquarePulseScalable
+from .square_pulse_legacy import SquarePulseLegacy
