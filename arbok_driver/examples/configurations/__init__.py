fbf2785030a940fdb7811542b7a1335ee3d3761b
andncl <andreaskochnickl@web.de>
2026-01-21 06:54:40 +0000

    Added device config

--- arbok_driver/examples/configurations/__init__.py
+++ arbok_driver/examples/configurations/__init__.py
@@ -1,6 +1,7 @@
-from.sequence_configs.square_pulse_confs import (
+from .sequence_configs.square_pulse_confs import (
     square_pulse_conf,
     square_pulse_scalable_conf
 )
+from .sequence_configs.device_config import device_config
 from .hardware_configs.opx1000_config import opx1000_config
 from .hardware_configs.divider_config import divider_config
