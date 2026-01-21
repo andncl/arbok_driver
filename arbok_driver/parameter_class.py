5a79f5c97a2a41ba4bc93fc42fe18237b6f202fd
andncl <andreaskochnickl@web.de>
2026-01-21 03:09:34 +0000

    Made parameter class abstract and added len dunder method

--- arbok_driver/parameter_class.py
+++ arbok_driver/parameter_class.py
@@ -1,7 +1,11 @@
 """Module containing ParameterClass"""
+from abc import ABC
+from dataclasses import dataclass, fields
 
-class ParameterClass:
+@dataclass(frozen = True)
+class ParameterClass(ABC):
     """
     Parent for shared identity for the parameter classes every sub-sequence should own
     """
-    pass
+    def __len__(self) -> int:
+        return len(fields(self))
