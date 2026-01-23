f9f9ed02a9c6b5f2f3232cd5e77f7e603dc52aa9
andncl <andreaskochnickl@web.de>
2026-01-23 00:42:43 +0000

    Added coulomb peaks example config

--- arbok_driver/examples/configurations/__init__.py
+++ arbok_driver/examples/configurations/__init__.py
@@ -1,6 +1,9 @@
 from .sequence_configs.square_pulse_confs import (
     square_pulse_conf,
-    square_pulse_scalable_conf
+    square_pulse_scalable_conf,
+)
+from .sequence_configs.coulomb_peaks_config import (
+    coulomb_peaks_set1_set2_conf
 )
 from .sequence_configs.device_config import device_config
 from .hardware_configs.opx1000_config import opx1000_config
