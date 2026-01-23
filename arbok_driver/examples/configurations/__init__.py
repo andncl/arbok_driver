5521a45b3d932f647cfdceae16b7f6ffb89a0374
andncl <andreaskochnickl@web.de>
2026-01-23 00:54:59 +0000

    Added stability map config

--- arbok_driver/examples/configurations/__init__.py
+++ arbok_driver/examples/configurations/__init__.py
@@ -2,9 +2,9 @@ from .sequence_configs.square_pulse_confs import (
     square_pulse_conf,
     square_pulse_scalable_conf,
 )
-from .sequence_configs.coulomb_peaks_config import (
-    coulomb_peaks_set1_set2_conf
-)
+from .sequence_configs.coulomb_peaks_config import coulomb_peaks_set1_set2_conf
+from .sequence_configs.stability_map_config import stability_map_config
+
 from .sequence_configs.device_config import device_config
 from .hardware_configs.opx1000_config import opx1000_config
 from .hardware_configs.divider_config import divider_config
