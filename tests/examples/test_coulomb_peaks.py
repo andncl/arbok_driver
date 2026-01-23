fd2b09b11d15121509b8cf47940a5e30ae838116
andncl <andreaskochnickl@web.de>
2026-01-23 01:04:52 +0000

    Added assertions to new tests

--- tests/examples/test_coulomb_peaks.py
+++ tests/examples/test_coulomb_peaks.py
@@ -8,4 +8,6 @@ def test_coulomb_peaks(dummy_measurement):
         dummy_measurement, 'coulomb_peaks', coulomb_peaks_set1_set2_conf
     )
     prg_str = dummy_measurement.get_qua_program_as_str(recompile = True)
-    print(prg_str)
+    assert len(coulomb_peaks.signals) == 3
+    assert len(coulomb_peaks.abstract_readouts) == 3
+    assert len(coulomb_peaks.readout_groups) == 2
