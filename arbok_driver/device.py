918809489f28780166f35f97b5795e4a4535416f
andncl <andreaskochnickl@web.de>
2026-03-26 07:13:22 +0000

    Simplified device

--- arbok_driver/device.py
+++ arbok_driver/device.py
@@ -6,15 +6,15 @@ class Device():
     """
     Class describing the used device by its config and the used sequence. 
     """
-
-    _master_config_path = None
-    master_config = None
+    _master_config: dict
+    _param_config: dict
+    _default_sequence_configs: dict
 
     def __init__(
             self, name: str,
             opx_config: dict,
             divider_config: dict,
-            master_config = None,
+            master_config: dict | None = None,
             ):
         """
         Constructor class for 'Device' class.
@@ -31,11 +31,15 @@ class Device():
         self.config = opx_config
         self.elements = list(self.config['elements'].keys())
         self.divider_config = divider_config
-        self._update_master_config(master_config)
-        self.master_config: dict = master_config
+        self.master_config = master_config
         self.sub_device_name = None
 
-    def _update_master_config(self, master_config = None):
+    @property
+    def master_config(self) -> dict:
+        return self._master_config
+    
+    @master_config.setter
+    def master_config(self, master_config: dict | None) -> None:
         """
         Update the master config from a dict
 
@@ -47,53 +51,19 @@ class Device():
         if master_config is None:
             master_config = {}
         if 'parameters' in master_config:
-            self.param_config = master_config['parameters']
+            self._param_config = master_config['parameters']
         else:
-            self.param_config = {}
+            self._param_config = {}
         if 'default_sequence_configs' in master_config:
-            self.default_sequence_configs = master_config['default_sequence_configs']
+            self._default_sequence_configs = master_config['default_sequence_configs']
         else:
-            self.default_sequence_configs = {}
+            self._default_sequence_configs = {}
+        self._master_config = master_config
 
     @property
-    def master_config_path(self):
-        """
-        Getter for master_config.
-
-        Returns:
-            str: The current configuration dictionary.
-        """
-        return self._master_config_path
-
-    @master_config_path.setter
-    def master_config_path(self, config_path):
-        """
-        Setter for master_config_path.
-        Validates and sets the configuration path as a string.
-
-        Args:
-            config_path (str): A dictionary containing configuration data.
-
-        Raises:
-            ValueError: If the config is not a string.
-        """
-        if not isinstance(config_path, str):
-            raise ValueError("master_config_path must be a str.")
-        self._master_config_path = config_path
-        mc = get_module('mc', config_path)
-        if not hasattr(mc, 'config'):
-            raise AttributeError(
-                "Dictionary 'config' not found in the file: "
-                f"{self._master_config_path}"
-                )
-        self._update_master_config(mc.config)
-
-    def reload_master_config(self):
-        """
-        If the master config path is set, force reloading
-        """
-        if self._master_config_path is not None:
-            mcp = self.master_config_path
-            self.master_config_path = mcp
-        else:
-            return self.master_config
+    def param_config(self):
+        return self._param_config
+    
+    @property
+    def default_sequence_configs(self):
+        return self._default_sequence_configs
